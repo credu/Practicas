e8dff32f99b7f53e1e23b38b954165b00796f721
Credu <jesusjeampoolm@gmail.com>
2020-10-19 12:30:00 -0500

    reorganized

--- Lenguajes/c++/prueba2 copy.cpp
+++ Lenguajes/c++/prueba2 copy.cpp
@@ -6,8 +6,8 @@ int main()
     int n1;
     int n2;
     printf("Introduce un numero: ");
-    scanf_s("%i", &n1);
+    scanf("%i", &n1);
     printf("Introduce un numero: ");
-    scanf_s("%i", &n2);
+    scanf("%i", &n2);
     printf("\nLa suma de los dos numeros es: %i\n", n1+n2);
 }
