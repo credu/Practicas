512039b2e8324ee235cdab5a46e7bbc34b39b882
Jesus Mendoza <ccredulidad@gmail.com>
2023-08-22 10:11:57 -0500

    Unnecesary files removed in c# folder

---
+++ C#/OfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuantos años tienes?");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18) Console.WriteLine("Eres mayor de edad");
+            else Console.WriteLine("Eres menor de edad");
+        }
+    }
+}
---
+++ C#/PracticaMetodos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace PracticaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe dos numeros en dos lineas");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            SumaDeNumeros(n1, n2);
+        }
+
+        static void SumaDeNumeros(int num1, int num2)=> Console.WriteLine($"La suma de los 2 numeros es: {num1+num2}");
+    }
+}
